fc1534d6729ecc6d3195e60f16179e8d9869173d
ruturaj4 <ruturajkvaidya@ku.edu>
2018-11-03 19:43:18 -0500

    Created a table

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 # This is the main file
 
 import re
+import csv
 
 # For tab completion
 import readline
 readline.parse_and_bind("tab: complete")
-
 
 def readFile():
     f = input("Please enter the name of the input file: ")
@@ -16,19 +16,17 @@ def readFile():
         for line in filename:
             # Remove all the comments
             line = re.sub(re.compile("!.*?\n"), "", line)
-            if line != "":
+            # Remove the <> line
+            line = re.sub(re.compile("<[^>]+>"), "", line)
+            line = line.split()
+            if line != []:
                 inputFile.append(line)
     return inputFile
 
-#def scanFile(inputFile):
-    
+#def scanFile(inputFile): 
 
 def main():
     inputFile = readFile()
-    # Remove the <> line (which will be first 1 in this case)
-    inputFile = inputFile[1:]
-    # Remove all the blank lines
-    inputFile = "".join(str(x) for x in inputFile)
     print(inputFile)
 
 if __name__=="__main__":
